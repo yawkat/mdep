ae11c2ef2109ff5acbcfaad893071c688331bcf8
Jonas Konrad <me@yawk.at>
2015-08-31 15:53:29 +0200

    Fix snapshot resolving

--- maven-plugin/src/main/java/at/yawk/mdep/GenerateMojo.java
+++ maven-plugin/src/main/java/at/yawk/mdep/GenerateMojo.java
@@ -2,13 +2,13 @@ package at.yawk.mdep;
 
 import at.yawk.mdep.model.Dependency;
 import at.yawk.mdep.model.DependencySet;
+import com.google.common.annotations.VisibleForTesting;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
@@ -18,6 +18,10 @@ import javax.xml.bind.Marshaller;
 import lombok.SneakyThrows;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.metadata.ArtifactRepositoryMetadata;
+import org.apache.maven.artifact.repository.metadata.Metadata;
+import org.apache.maven.artifact.repository.metadata.Snapshot;
+import org.apache.maven.artifact.repository.metadata.io.xpp3.MetadataXpp3Reader;
 import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -27,6 +31,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 /**
  * @author yawkat
@@ -59,7 +64,6 @@ public class GenerateMojo extends AbstractMojo {
     Set<String> repositories = null;
 
     @Override
-    @SneakyThrows({ MalformedURLException.class, NoSuchAlgorithmException.class })
     public void execute() throws MojoExecutionException, MojoFailureException {
         ArtifactMatcher includesMatcher;
         if (includes == null) {
@@ -68,53 +72,19 @@ public class GenerateMojo extends AbstractMojo {
             includesMatcher = ArtifactMatcher.anyMatch(toAntMatchers(includes));
         }
         ArtifactMatcher excludesMatcher = ArtifactMatcher.anyMatch(toAntMatchers(excludes));
-
         ArtifactMatcher matcher = includesMatcher.and(excludesMatcher.not());
 
         List<Dependency> dependencies = new ArrayList<>();
-
         for (Artifact artifact : dependencyArtifacts) {
             // only include compile dependencies
             // todo: configurable
             if (!artifact.getScope().equals(Artifact.SCOPE_COMPILE)) { continue; }
 
-            if (!matcher.matches(artifact)) { continue; }
-
-            boolean foundInRepository = false;
-            for (ArtifactRepository repository : remoteArtifactRepositories) {
-                // only scan configured repositories
-                if (this.repositories != null &&
-                    !this.repositories.contains(repository.getId())) {
-                    continue;
-                }
-
-                URL url = new URL(repository.getUrl() + '/' + repository.pathOf(artifact));
-                try (InputStream input = url.openStream()) {
-                    getLog().info("Getting checksum for " + artifact);
-
-                    MessageDigest digest = MessageDigest.getInstance("SHA-512");
-                    byte[] buf = new byte[4096];
-                    int len;
-                    while ((len = input.read(buf)) >= 0) {
-                        digest.update(buf, 0, len);
-                    }
-
-                    Dependency dependency = new Dependency();
-                    dependency.setUrl(url);
-                    dependency.setSha512sum(digest.digest());
-                    dependencies.add(dependency);
-
-                    // added dependency correctly - don't check the other repos
-                    foundInRepository = true;
-                    break;
-                } catch (IOException ignored) {
-                    // skip this repo
-                }
+            if (!matcher.matches(artifact)) {
+                continue;
             }
 
-            if (!foundInRepository) {
-                throw new MojoExecutionException("Could not find " + artifact + " in configured repositories");
-            }
+            dependencies.add(findArtifact(artifact));
         }
 
         getLog().info("Saving dependency xml");
@@ -139,6 +109,111 @@ public class GenerateMojo extends AbstractMojo {
         resource.setDirectory(outputDirectory.toString());
         resource.setFiltering(false);
         project.addResource(resource);
+    }
+
+    private Dependency findArtifact(Artifact artifact) throws MojoExecutionException {
+        for (ArtifactRepository repository : remoteArtifactRepositories) {
+            // only scan configured repositories
+            if (this.repositories != null &&
+                !this.repositories.contains(repository.getId())) {
+                continue;
+            }
+
+            Dependency dependency = findArtifactInRepository(artifact, repository);
+            if (dependency != null) { return dependency; }
+        }
+
+        throw new MojoExecutionException("Could not find " + artifact + " in configured repositories");
+    }
+
+    @Nullable
+    @SneakyThrows//({ MalformedURLException.class, NoSuchAlgorithmException.class })
+    @VisibleForTesting
+    Dependency findArtifactInRepository(Artifact artifact, ArtifactRepository repository)
+            throws MojoExecutionException {
+
+        String artifactPath = getArtifactPath(artifact, artifact.getVersion());
+        if (artifact.isSnapshot()) {
+            ArtifactRepositoryMetadata metadata = new ArtifactRepositoryMetadata(artifact) {
+                // maven is weird - i have yet to find a better solution.
+
+                @Override
+                public boolean storedInArtifactVersionDirectory() {
+                    return true;
+                }
+
+                @Override
+                public String getBaseVersion() {
+                    return artifact.getBaseVersion();
+                }
+            };
+
+            // try to load maven-metadata.xml in case we need to use a different version for snapshots
+            URL metaUrl = new URL(repository.getUrl() + '/' + repository.pathOfRemoteRepositoryMetadata(metadata));
+
+            Metadata loadedMetadata;
+            try (InputStream input = openStream(metaUrl)) {
+                loadedMetadata = new MetadataXpp3Reader().read(input, true);
+            } catch (IOException e) {
+                // could not find metadata
+                loadedMetadata = null;
+            } catch (XmlPullParserException e) {
+                throw new MojoExecutionException("Failed to parse metadata", e);
+            }
+
+            if (loadedMetadata != null) {
+                Snapshot snapshot = loadedMetadata.getVersioning().getSnapshot();
+
+                String versionWithoutSuffix = artifact.getVersion()
+                        .substring(0, artifact.getBaseVersion().lastIndexOf('-'));
+                artifactPath = getArtifactPath(artifact,
+                                               versionWithoutSuffix + '-' + snapshot.getTimestamp() + '-' +
+                                               snapshot.getBuildNumber());
+            }
+        }
+
+        URL url = new URL(repository.getUrl() + '/' + artifactPath);
+        try (InputStream input = openStream(url)) {
+            getLog().info("Getting checksum for " + artifact);
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-512");
+            byte[] buf = new byte[4096];
+            int len;
+            while ((len = input.read(buf)) >= 0) {
+                digest.update(buf, 0, len);
+            }
+
+            Dependency dependency = new Dependency();
+            dependency.setUrl(url);
+            dependency.setSha512sum(digest.digest());
+            return dependency;
+        } catch (IOException ignored) {
+            // not in this repo
+            return null;
+        }
+    }
+
+    private static String getArtifactPath(Artifact artifact, String version) {
+        StringBuilder builder = new StringBuilder()
+                .append(artifact.getGroupId().replace('.', '/')).append('/')
+                .append(artifact.getArtifactId()).append('/')
+                .append(artifact.getBaseVersion()).append('/')
+                .append(artifact.getArtifactId()).append('-').append(version);
+        if (artifact.getArtifactHandler().getClassifier() != null) {
+            builder.append('-').append(artifact.getArtifactHandler().getClassifier());
+        }
+        String extension = artifact.getArtifactHandler().getExtension();
+        if (extension == null) {
+            extension = "jar";
+        }
+        return builder.append('.').append(extension).toString();
+    }
+
+    private static InputStream openStream(URL metaUrl) throws IOException {
+        URLConnection urlConnection = metaUrl.openConnection();
+        // Java user agent is blocked in some cases
+        urlConnection.setRequestProperty("User-Agent", "mdep");
+        return urlConnection.getInputStream();
     }
 
     private static List<ArtifactMatcher> toAntMatchers(List<String> antPatterns) {
---
+++ maven-plugin/src/test/java/at/yawk/mdep/GenerateMojoTest.java
@@ -0,0 +1,34 @@
+package at.yawk.mdep;
+
+import at.yawk.mdep.model.Dependency;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
+import org.apache.maven.artifact.repository.MavenArtifactRepository;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.SystemStreamLog;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author yawkat
+ */
+public class GenerateMojoTest {
+    @Test
+    public void testFindArtifactInRepository() throws MojoExecutionException {
+        GenerateMojo mojo = new GenerateMojo();
+        mojo.setLog(new SystemStreamLog());
+
+        Dependency dependency = mojo.findArtifactInRepository(
+                new DefaultArtifact("org.spigotmc", "spigot-api", "1.8-R0.1-SNAPSHOT",
+                                    "compile", "jar", "jar", new DefaultArtifactHandler()),
+                new MavenArtifactRepository("yawkat",
+                                            "http://mvn.yawk.at",
+                                            new DefaultRepositoryLayout(),
+                                            new ArtifactRepositoryPolicy(),
+                                            new ArtifactRepositoryPolicy())
+        );
+        Assert.assertNotNull(dependency);
+    }
+}
